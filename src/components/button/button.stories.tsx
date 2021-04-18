4ee7af924486838fd75f8a5612acb48cbba79c6f
ciemna_noc <agarkowa@gmail.com>
2021-04-18 02:10:17 +0200

    initial commit

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,45 +1,50 @@
 import * as React from 'react';
-import { Story } from '@storybook/react/types-6-0';
+import {Story} from '@storybook/react/types-6-0';
 import 'antd/lib/button/style/index.css';
 import Button from './index';
 
-import {
-  Title,
-  Subtitle,
-  Description,
-  Primary,
-  ArgsTable,
-  Stories,
-  PRIMARY_STORY
-} from '@storybook/addon-docs/blocks';
-import { xs } from '../../config/breakpoints';
+import {ArgsTable, Description, Primary, PRIMARY_STORY, Stories, Subtitle, Title} from '@storybook/addon-docs/blocks';
+import {xs} from '../../config/breakpoints';
 
 export default {
-  title: 'Components/Button',
+  title: 'Components/Кнопка',
   component: Button,
+  decorators: [(Story) => <div style={{margin: '3em'}}><Story /></div>],
+  argTypes: { onClick: { action: 'clicked' } },
   parameters: {
     docs: {
       page: () => (
         <>
           <Title />
-          <Subtitle />
+          <Subtitle><a href="#">Ссылка на макет</a></Subtitle>
           <Description>
-            {`Стандартная кнопка из библиотека antd, которая отстилизована согласно макету. Помимо
-                  стандартных props, появился "color", который согласно цветовой схеме макета добавляет нужный цвет.
-                  На разрешении экрана ${xs} и ниже, кнопка занимает всю ширину экрана "width: 100%"`}
+            {`Кнопка которая запускает действие. Помимо стандартных возможностей допускается кастомный цвет.
+              На разрешении экрана ${xs} и ниже, кнопка занимает всю ширину экрана "width: 100%"`}
           </Description>
           <Primary />
+          <Description>
+            {`Создана на базе кнопки из библиотеки Antd.`}
+          </Description>
           <ArgsTable story={PRIMARY_STORY} />
           <Stories />
+
         </>
       )
-    }
+    },
   }
 };
 
 const Template: Story = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
+export const Large = Template.bind({});
+
+Large.args = {
+  type: 'primary',
+  danger: false,
+  children: 'Кнопка',
+  loading: false,
+}
 
 Default.args = {
   type: 'primary',
