6e36fd1facef7af915e73f19babf82ae68057d07
ciemna_noc <agarkowa@gmail.com>
2021-05-13 14:35:10 +0200

    rq-type

---
+++ src/components/rqType/styled.tsx
@@ -0,0 +1,21 @@
+import { Form, Radio } from 'antd';
+import styled from 'styled-components';
+import { xs } from '../../config/breakpoints';
+
+export const StyledRadioGroup = styled(Radio.Group)`
+  @media (max-width: ${xs}) {
+    display: flex;
+    flex-direction: column;
+  }
+  .ant-radio-inner {
+    width: 20px;
+    height: 20px;
+
+    &:after {
+      width: 12px;
+      height: 12px;
+      top: 3.2px;
+      left: 2.9px;
+    }
+  }
+`;
