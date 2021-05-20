bdeb7b48921ff06de8c68768ed06d9be20187861
ciemna_noc <agarkowa@gmail.com>
2021-05-20 14:07:14 +0200

    tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const { defaults } = require('jest-config');
 
 module.exports = {
   preset: 'ts-jest',
+  modulePaths: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'node',
   verbose: true,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
@@ -9,7 +10,8 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/tests/__mocks__/cssMock.js'
+    '\\.(css|less)$': '<rootDir>/tests/__mocks__/cssMock.js',
+    'src/(.*)': '<rootDir>/src/$1'
   },
   transformIgnorePatterns: ['node_modules/[^/]+?/(?!(es|node_modules)/)']
 };
