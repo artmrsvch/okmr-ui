ae98e1bf00ffa91a8a2f73d3e13091f367f471df
Артем Забиров <artemzabirov@MacBook-Pro-Artem.local>
2021-05-26 11:03:09 +0300

    fixed: bugs

--- src/components/button/customized.tsx
+++ src/components/button/customized.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import {StyledAtndButton as Button, StyledAtndButtonProps} from "./styled";
+import { Button } from '../index';
 
-export interface CustomizedButtonProps extends StyledAtndButtonProps {
+export interface CustomizedButtonProps {
   suffix?: React.ReactNode | string;
   prefix?: React.ReactNode | string;
   children: string | React.ReactNode;
-  className? : string;
+  className?: string;
 }
 
-const CustomizedButton: React.FC<CustomizedButtonProps> = ({suffix, prefix, children, ...restProps}) => (
-
-  <Button
-    icon={prefix}
-    {...restProps}
-  >
+const CustomizedButton: React.FC<CustomizedButtonProps> = ({
+  suffix,
+  prefix,
+  children,
+  ...restProps
+}) => (
+  <Button icon={prefix} {...restProps}>
     {children || null}
     {suffix}
-  </Button>)
+  </Button>
+);
 
 export default CustomizedButton;
